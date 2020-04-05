90d17c160d7123984b3277b83bfbac7c987e2b70
wertrain <wertrain@gmail.com>
2020-04-05 21:32:47 +0900

    main を追加

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+import MeCab
+import collections
+# MeCabリソースファイルへのパスを通す
+os.environ['MECABRC'] = r'.\venv\Scripts\etc\mecabrc'
+
+def main():
+    return
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
