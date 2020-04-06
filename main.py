41efc2d3075b0f5259964cd830645d99dc9d2240
wertrain <wertrain@gmail.com>
2020-04-07 01:12:15 +0900

    WIP

--- main.py
+++ main.py
@@ -3,17 +3,33 @@ import os
 import MeCab
 import collections
 import argparse
+import json
+import pickle
 # MeCabリソースファイルへのパスを通す
 os.environ['MECABRC'] = r'.\venv\Scripts\etc\mecabrc'
+
+def pickle_dump(file, data):
+    f = open(file, 'wb')
+    pickle.dump(data, f)
+    f.close()
+
+def pickle_load(file):
+    f = open(file, 'rb')
+    data = pickle.load(f)
+    f.close()
+    return data
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="target file path", type=str)
+    parser.add_argument("--format", type=str)
+    parser.add_argument("--input", type=str)
     parser.add_argument("--output", type=str)
     args = parser.parse_args()
-    print (args)
 
-    if(not os.path.exists(args.file)):
+    if (not os.path.exists(args.file)):
+        sys.exit(1)
+    if (args.input != None and not os.path.exists(args.file)):
         sys.exit(1)
 
     fileobj = open(args.file)
@@ -49,6 +65,22 @@ def main():
                     word_list.append(token)
                     words[lemma] = word_list
         node = node.next
+
+    result = []
+    for word in list(words.values()):
+        result.extend(word)
+
+    if (args.format == 'json'):
+        print(json.dumps (result))
+    elif (args.format == 'text'):
+        print(result)
+
+    data = []
+    if (args.input != None):
+         data = pickle_load(args.input)
+    data.extend(result)
+    if (args.output != None):
+        pickle_dump(args.output, data)
 
 if __name__ == "__main__":
     # execute only if run as a script
