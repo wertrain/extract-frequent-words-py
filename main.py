55864cffec6861de733aa25cb8430aef38a9610c
wertrain <wertrain@gmail.com>
2020-04-12 18:48:47 +0900

    マージ処理の修正など

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 import json
 import pickle
 # MeCabリソースファイルへのパスを通す
+#os.environ['MECABRC'] = r'.\etc\mecabrc'
 os.environ['MECABRC'] = r'.\venv\Scripts\etc\mecabrc'
 
 def pickle_dump(file, data):
@@ -21,20 +22,17 @@ def pickle_load(file):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="target file path", type=str)
-    parser.add_argument("--format", type=str)
+    parser.add_argument("target", help="target file or text", type=str)
+    parser.add_argument("--format", help="'text' or 'json'", type=str)
     parser.add_argument("--input", type=str)
     parser.add_argument("--output", type=str)
     args = parser.parse_args()
 
-    if (not os.path.exists(args.file)):
-        sys.exit(1)
-    if (args.input != None and not os.path.exists(args.file)):
-        sys.exit(1)
-
-    fileobj = open(args.file)
-    text = fileobj.read()
-    fileobj.close()
+    text = args.target
+    if (os.path.exists(args.target)):
+        fileobj = open(args.target)
+        text = fileobj.read()
+        fileobj.close()
 
     mecab = MeCab.Tagger("-Ochasen")
     node = mecab.parseToNode(text)
@@ -66,21 +64,37 @@ def main():
                     words[lemma] = word_list
         node = node.next
 
+    # マージ
+    if (args.input != None and os.path.exists(args.input)):
+         data_list = pickle_load(args.input)
+         for data in data_list:
+            new_word = True
+            word_list = []
+            # 辞書に既に登録されているか調べる
+            if (data['lemma'] in words):
+                # 辞書にはリストで保存されている
+                word_list = words[data['lemma']]
+                for word in word_list:
+                    # カテゴリが一致するなら同一と見なす
+                    if data['category'] == word['category']:
+                        word['count'] = data['count'] + word['count']
+                        new_word = False
+                        break
+            if new_word:
+                word_list.append(data)
+                words[data['lemma']] = word_list
+            
     result = []
     for word in list(words.values()):
         result.extend(word)
 
     if (args.format == 'json'):
         print(json.dumps (result))
-    elif (args.format == 'text'):
+    elif (args.format == 'text' or args.format == None):
         print(result)
 
-    data = []
-    if (args.input != None):
-         data = pickle_load(args.input)
-    data.extend(result)
     if (args.output != None):
-        pickle_dump(args.output, data)
+        pickle_dump(args.output, result)
 
 if __name__ == "__main__":
     # execute only if run as a script
